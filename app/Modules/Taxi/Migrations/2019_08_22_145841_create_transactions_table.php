d86f73e2589792460dac4f319758fe8f6cf20999
wesley <wesley@onpoint.digital>
2019-08-22 17:03:18 +0200

    +ADD: Migrations

---
+++ app/Modules/Taxi/Migrations/2019_08_22_145841_create_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->enum('type', ['add', 'subtract']);
+            $table->double('amount')->default(0);
+            $table->ipAddress('from_ip');
+            $table->bigInteger('budget_id')->unsigned();
+            $table->foreign('budget_id')->references('id')->on('budgets');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
